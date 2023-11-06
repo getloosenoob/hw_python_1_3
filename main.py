bbb44819c7da9a50e4b845a37c7c2cc8e46020b7
getloosenoob <ka88@mail.ru>
2023-11-06 14:09:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+task_dict = {}
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
